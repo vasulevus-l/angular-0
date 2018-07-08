1594d942bab7217bdc261d3660a3d15ef2057f6f
Olexander <vasulevus@gmail.com>
2018-07-08 17:46:06 +0300

    poku vse

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core';  //імпортуємо необхідний
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+@Component({                                //метадані та шаблон містяться в декораторі @Component
+  selector: 'app-root',                     //
+  templateUrl: './app.component.html',      //
+  styleUrls: ['./app.component.css']        //
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent {                 //експортуємо клас typescript для того, щоб він був доступний і іншим компонентам.
+  title:string = 'app';                     //typescript strong typing визначає клас.
 }
